89ba3cceef6f8ff5d098f1330b09dfcbfd636c6d
jeremiasleite <jeremias23456@gmail.com>
2020-05-19 21:52:41 -0300

    add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,30 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import helmet from 'helmet'
+import dotenv from "dotenv";
 
 import userRouter from './routes/UserRouter'
 
 class App {
     public express: express.Application
-
-    constructor() {
+    
+    constructor() {        
         this.express = express()
+        dotenv.config();
         this.middlewares()
-        this.routes()
+        this.routes()        
     }
 
-    private middlewares (): void{
+    private middlewares (): void{        
+        this.express.use(helmet())
         this.express.use(express.json())
-        this.express.use(cors())
-        this.database()
+        this.express.use(cors())        
+        this.database()        
     }
 
     private database (): void {
-        mongoose.connect('', {
+        mongoose.connect(process.env.URL_DATABASE, {
             useNewUrlParser : true
         })
     }
